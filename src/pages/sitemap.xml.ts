254b478a0afda16fa7da6f9cee5ea8eafc3b0a5f
Saki chan <125722780+belleangelina@users.noreply.github.com>
2026-07-04 23:29:47 +0800

    site: add sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,25 @@
+import { getSitemapUrls } from '../lib/content';
+
+function escapeXml(value: string): string {
+  return value
+    .replace(/&/g, '&amp;')
+    .replace(/</g, '&lt;')
+    .replace(/>/g, '&gt;')
+    .replace(/"/g, '&quot;')
+    .replace(/'/g, '&apos;');
+}
+
+export async function GET() {
+  const urls = await getSitemapUrls();
+  const entries = urls.map((url) => `  <url><loc>${escapeXml(url)}</loc></url>`).join('\n');
+  const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+${entries}
+</urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      'Content-Type': 'application/xml; charset=utf-8'
+    }
+  });
+}
